b78a7061dfb76784ecbaaf54bd09af54a4ef15ab
Matthew Holder <sixxgate@hotmail.com>
2021-01-14 21:02:35 -0600

    Major build system replacement and remove browser element to ensure CJS or ESM usage

--- build/roller.ts
+++ build/roller.ts
@@ -1,184 +1,112 @@
-import type { InputOptions, OutputOptions, RollupOptions } from "rollup";
-import type { MergeStrategies } from "./merge";
-import { ignore, merge, mergeInput, mergeMaybeArray, replace } from "./merge";
-import type { SimpleGlobals } from "./utils";
-import { makeExternals, msg } from "./utils";
+import type { RollupOptions } from "rollup";
+import type { ReadonlyDeep } from "type-fest";
+import type {
+    BuildVariables,
+    Configuration,
+    RollerContext,
+    RollerContextData,
+    Target,
+    RollerContextBase,
+} from "./build-base";
+import { coreStrategies, inputStrategies, outputStrategies } from "./build-base";
+import { merge } from "./merge";
+import type { RollerPlugin } from "./plugins";
+import { defaultPlugins } from "./plugins/builtin-plugins";
+import { makeExternals, readPackageConfig } from "./utils";
 
-export type Targets = "dev" | "prod";
-export type Configuration = { target: Targets; build: string };
-export type Conditional = (config: Configuration) => boolean;
-export type GlobalsCallback = () => SimpleGlobals;
-export type InputCallback = () => InputOptions;
-export type OutputCallback = () => OutputOptions;
-export type OverrideCallback = () => RollupOptions;
+export type { BuildVariables, Configuration, RollerContext, RollerContextData, Target } from "./build-base";
+export { RollerPlugin } from "./plugins";
 
-/** Contains the merge strategy for roller configuration. Anything not listed is automatically `ignore`. */
-const coreStrategies: MergeStrategies = {
-    external: ignore,
-    input:    mergeInput,
-    plugins:  mergeMaybeArray,
-    onwarn:   replace,
-    output:   {
-        dir:                     replace,
-        file:                    replace,
-        format:                  replace,
-        globals:                 ignore,
-        name:                    replace,
-        plugins:                 mergeMaybeArray,
-        assetFileNames:          replace,
-        banner:                  replace,
-        footer:                  replace,
-        chunkFileNames:          replace,
-        compact:                 replace,
-        entryFileNames:          replace,
-        extend:                  replace,
-        hoistTransitiveImports:  replace,
-        inlineDynamicImports:    replace,
-        interop:                 replace,
-        intro:                   replace,
-        outro:                   replace,
-        manualChunks:            replace,
-        minifyInternalExports:   replace,
-        paths:                   replace,
-        preserveModules:         replace,
-        preserveModulesRoot:     replace,
-        sourcemap:               ignore,
-        sourcemapExcludeSources: replace,
-        sourcemapFile:           replace,
-        sourcemapPathTransform:  replace,
-    },
-    preserveEntrySignatures: replace,
-    strictDeprecations:      replace,
+function makeDefaultContextData(variables: BuildVariables): RollerContextData {
+    return {
+        input: {
+            input: variables.inPath,
+        },
+        output: {
+            name:      variables.name,
+            file:      variables.outPath,
+            sourcemap: true,
+            format:    variables.target,
+        },
+        globals:   { },
+        overrides: { },
+    };
+}
+
+function makeContext(data: RollerContextData): RollerContextBase {
+    return {
+        globals: globals => {
+            data.globals = { ...data.globals, ...globals };
+        },
+        input: input => {
+            merge(inputStrategies, data.input, input);
+        },
+        output: output => {
+            merge(outputStrategies, data.output, output);
+            if (output.dir) {
+                delete data.output.file;
+            }
+        },
+        override: overrides => {
+            merge(coreStrategies, data.overrides, overrides);
+        },
+    };
+}
+
+export type BuildConfiguration = (variables: ReadonlyDeep<BuildVariables>, roll: RollerContext) => void;
+
+interface Roller {
+    (config: BuildConfiguration): Promise<RollupOptions[]>;
+    extend: (plugin: RollerPlugin) => void;
+}
+
+/** Plug-in registry, with defaults. */
+// NOTE: This order is important.
+const plugins: RollerPlugin[] = [...defaultPlugins];
+
+const roller: Roller = async (config: BuildConfiguration): Promise<RollupOptions[]> => {
+    const pkgConfig = await readPackageConfig();
+
+    const results: RollerContextData[] = [];
+    for (const [ target, outPath ] of Object.entries(pkgConfig.outputs) as [ Target, string ][]) {
+        const variables: ReadonlyDeep<BuildVariables> = Object.freeze({
+            configuration: (process.env.CONFIGURATION || "dev") as Configuration,
+            target,
+            env:           process.env,
+            name:          pkgConfig.name,
+            inPath:        pkgConfig.input,
+            outPath,
+            typings:       pkgConfig.typings,
+        });
+
+        const data = makeDefaultContextData(variables);
+        const context = makeContext(data) as RollerContext;
+        for (const plugin of plugins) {
+            plugin.prepare(context, variables);
+        }
+
+        config(variables, context);
+
+        for (const plugin of plugins) {
+            if (plugin.enabled(data, variables)) {
+                data.globals = { ...data.globals, ...plugin.globals() };
+                data.input = merge(inputStrategies, data.input, plugin.input());
+                data.output = merge(outputStrategies, data.output, plugin.output());
+                data.overrides = merge(coreStrategies, data.overrides, plugin.overrides());
+            }
+        }
+
+        if (data.output.format === "umd" || data.output.format === "iife") {
+            data.output.globals = { ...data.globals };
+        }
+
+        data.input.external = makeExternals(data.globals);
+        results.push(data);
+    }
+
+    return results.map(result => merge(coreStrategies, { ...result.input, output: result.output }, result.overrides));
 };
 
-/** Roller configuration context */
-class RollerContext {
-    globals: SimpleGlobals;
-    config: RollupOptions;
+roller.extend = plugin => plugins.push(plugin);
 
-    constructor() {
-        this.globals = {};
-        this.config = { output: { sourcemap: true } };
-    }
-
-    merge(data: RollupOptions): this {
-        merge(coreStrategies, this.config, data);
-
-        return this;
-    }
-}
-
-/** Rollup configuration builder */
-export default class Roller {
-    private readonly configurations: Configuration[];
-    private readonly target = (process.env.TARGET || "dev") as Targets;
-    private readonly builds = new Map<string, Set<string>>();
-    private readonly globalsCallbacks = new Map<Conditional, GlobalsCallback>();
-    private readonly inputCallbacks = new Map<Conditional, InputCallback>();
-    private readonly outputCallbacks = new Map<Conditional, OutputCallback>();
-    private readonly overrideCallbacks = new Map<Conditional, OverrideCallback>();
-
-    constructor(configurations: Configuration[]) {
-        this.configurations = configurations;
-
-        this.generateBuildSets();
-    }
-
-    globals(condition: Conditional, globals: GlobalsCallback): this {
-        this.globalsCallbacks.set(condition, globals);
-
-        return this;
-    }
-
-    input(condition: Conditional, input: InputCallback): this {
-        this.inputCallbacks.set(condition, input);
-
-        return this;
-    }
-
-    output(condition: Conditional, output: OutputCallback): this {
-        this.outputCallbacks.set(condition, output);
-
-        return this;
-    }
-
-    override(condition: Conditional, override: OverrideCallback): this {
-        this.overrideCallbacks.set(condition, override);
-
-        return this;
-    }
-
-    build(): RollupOptions[] {
-        const builds = this.builds.get(this.target) || null;
-        if (builds === null || builds.size === 0) {
-            throw new Error(msg(`Nothing configured for ${this.target}`));
-        }
-
-        /** @type {RollerContext[]} */
-        const contexts = [];
-        for (const build of builds) {
-            const context = new RollerContext();
-            const config = { target: this.target, build };
-
-            for (const [ condition, callback ] of this.globalsCallbacks) {
-                if (condition(config)) {
-                    context.globals = { ...context.globals, ...callback() };
-                }
-            }
-
-            for (const [ condition, callback ] of this.inputCallbacks) {
-                if (condition(config)) {
-                    context.merge(callback());
-                }
-            }
-
-            for (const [ condition, callback ] of this.outputCallbacks) {
-                if (condition(config)) {
-                    context.merge({ output: callback() });
-                }
-            }
-
-            for (const [ condition, callback ] of this.overrideCallbacks) {
-                if (condition(config)) {
-                    context.merge(callback());
-                }
-            }
-
-            context.config.external = makeExternals(context.globals);
-
-            if (context.config.output) {
-                if (Array.isArray(context.config.output)) {
-                    throw new Error("Not expecting options to be an array");
-                }
-
-                if (context.config.output.format === "umd" || context.config.output.format === "iife") {
-                    context.config.output.globals = { ...context.globals };
-                }
-            }
-
-            contexts.push(context);
-        }
-
-        return contexts.map(context => context.config);
-    }
-
-    private generateBuildSets(): void {
-        for (const config of this.configurations) {
-            const builds = this.getBuildSet(config);
-
-            builds.add(config.build);
-        }
-    }
-
-    private getBuildSet(config: Configuration): Set<string> {
-        const set = this.builds.get(config.target) || null;
-        if (set !== null) {
-            return set;
-        }
-
-        const newSet = new Set<string>();
-        this.builds.set(config.target, newSet);
-
-        return newSet;
-    }
-}
+export default roller;
