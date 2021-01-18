71263cc1d608010d8ad5f219c2118a1df139030e
Matthew Holder <sixxgate@hotmail.com>
2021-01-18 00:03:51 -0600

    Switched to Rolete and fixed the test workflow

--- build/rollup.config.cjs
+++
@@ -1,9 +0,0 @@
-require("ts-node").register();
-
-try {
-    module.exports = require("../rollup.config");
-} catch (_error) {
-    const roller = require("./roller").default;
-
-    module.exports = roller(() => undefined);
-}
